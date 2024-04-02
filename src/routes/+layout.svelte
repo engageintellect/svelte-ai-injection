a3a80d30c4f7221cd9239728c1e0fd5ec3249173
engageintellect <jc9361@gmail.com>
2024-04-01 17:05:54 -0700

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,7 @@
 
 <div class="">
 	<Nav />
-	<div class="max-w-3xl mx-auto p-2">
+	<div class="">
 		<slot />
 	</div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,12 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { useChat } from 'ai/svelte';
-	import { goto } from '$app/navigation';
 	import { slide, fade } from 'svelte/transition';
+
 	import Icon from '@iconify/svelte';
 
 	const { input, handleSubmit, messages } = useChat();
 	let messagesEnd: HTMLElement;
-	let loading = false;
 
 	let welcomeMessage = {
 		id: 0,
@@ -28,9 +27,7 @@
 	}
 
 	function clearMessages() {
-		console.log('Messages before', $messages);
 		$messages = [];
-		console.log('Messages cleared', $messages);
 	}
 </script>
 
@@ -39,56 +36,58 @@
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<!-- {JSON.stringify($messages)} -->
-
-{#if $messages.length === 0}
-	<div
-		transition:slide={{ delay: 0, duration: 500 }}
-		class="flex items-center justify-center py-20"
-	>
-		<div out:fade={{ delay: 0, duration: 300 }} class="flex flex-col gap-2">
-			<div class="text-3xl">{welcomeMessage.title}</div>
-			<div class="text-xl font-thin">{welcomeMessage.heading}</div>
+<div class="flex flex-col w-full max-w-3xl mx-auto">
+	{#if $messages.length === 0}
+		<div
+			out:slide={{ delay: 0, duration: 500 }}
+			in:fade={{ delay: 0, duration: 1500 }}
+			class="flex-grow flex items-center justify-center p-5 py-10"
+		>
+			<div out:fade={{ delay: 0, duration: 1500 }} class="">
+				<div class="text-3xl">{welcomeMessage.title}</div>
+				<div class="text-xl font-thin">{welcomeMessage.heading}</div>
+			</div>
 		</div>
-	</div>
-{:else}
-	<div in:fade={{ delay: 0, duration: 1500 }} class="sticky top-[68px] -z-[-1]">
-		<button on:click={() => clearMessages()} class="btn flex items-center">
-			<div>Clear Chat</div>
-			<Icon icon="ri-close-circle-fill" class="w-7 h-7 text-error" />
-		</button>
-	</div>
-{/if}
+	{:else}
+		<div class="sticky top-[68px] z-10 p-2">
+			<div in:fade={{ delay: 0, duration: 500 }} class="inline-block">
+				<button on:click={clearMessages} class="btn flex items-center justify-center">
+					Clear Chat
+					<Icon icon="ri-close-circle-fill" class="w-7 h-7 text-error ml-2" />
+				</button>
+			</div>
+		</div>
 
-<!-- <div class="text-3xl mb-4">useChat</div> -->
-<div class="flex flex-col overflow-auto mb-auto" style="padding-bottom: 5rem;">
-	{#each $messages as message (message.id)}
-		<div class={message.role === 'assistant' ? 'chat chat-start' : 'chat chat-end'}>
-			<div class="chat-image avatar">
-				<div class="w-10 rounded-full">
-					<img
-						alt="Chat avatar"
-						src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-					/>
+		<div class="flex-grow overflow-auto pb-20 p-2">
+			{#each $messages as message (message.id)}
+				<div class={message.role === 'assistant' ? 'chat chat-start' : 'chat chat-end'}>
+					<div class="chat-image avatar">
+						<div class="w-10 rounded-full">
+							<img
+								alt="Chat avatar"
+								src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+							/>
+						</div>
+					</div>
+					<div class="chat-header capitalize">
+						{message.role}
+						<time class="text-xs opacity-50">12:45</time>
+					</div>
+					<div
+						class={message.role === 'assistant'
+							? 'chat-bubble bg-primary text-primary-content'
+							: 'chat-bubble'}
+					>
+						{message.content}
+					</div>
+					<div class="chat-footer opacity-50">Delivered</div>
 				</div>
-			</div>
-			<div class="chat-header">
-				{message.role}
-				<time class="text-xs opacity-50">12:45</time>
-			</div>
-			<div
-				class={message.role === 'assistant'
-					? 'chat-bubble bg-primary text-primary-content'
-					: 'chat-bubble'}
-			>
-				{message.content}
-			</div>
-			<div class="chat-footer opacity-50">Delivered</div>
+			{/each}
+			<div bind:this={messagesEnd}></div>
 		</div>
-	{/each}
-	<div bind:this={messagesEnd}></div>
+	{/if}
 </div>
-<form on:submit={handleSubmit} class="fixed inset-x-0 bottom-0 bg-base-300 py-4 px-4">
+<form on:submit={handleSubmit} class="fixed w-full mx-auto bottom-0 bg-base-300 py-4 px-4">
 	<div class="flex gap-2 items-center max-w-3xl mx-auto">
 		<input
 			autofocus
